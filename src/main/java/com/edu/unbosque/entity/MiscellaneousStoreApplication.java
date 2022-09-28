b8de45f7b97b3043e1900f5748b4ea45d458adee
MrFixThis <mrfixthis55@gmail.com>
2022-09-28 13:24:29 -0500

    first commit

---
+++ src/main/java/com/edu/unbosque/entity/MiscellaneousStoreApplication.java
@@ -0,0 +1,13 @@
+package com.edu.unbosque.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiscellaneousStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiscellaneousStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/unbosque/entity/MiscellaneousStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.unbosque.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiscellaneousStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
