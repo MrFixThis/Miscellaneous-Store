d86f38ab7f37ac6f18d24e0b76fcf8047d9de732
MrFixThis <mrfixthis55@gmail.com>
2022-09-28 19:09:09 -0500

    Raw classes added

--- src/main/java/com/edu/unbosque/MiscellaneousStoreApplication.java
+++ src/main/java/com/edu/unbosque/MiscellaneousStoreApplication.java
@@ -1,4 +1,4 @@
-package com.edu.unbosque.entity;
+package com.edu.unbosque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
