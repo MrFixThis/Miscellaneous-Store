0db478110f194a90ef70a6b4bae0ebea6e540d5a
MrFixThis <mrfixthis55@gmail.com>
2022-12-26 17:14:57 -0500

    v2.0: rest controllers created. Impl pendent

---
+++ src/main/java/com/store/controller/rest/MagazineLotController.java
@@ -0,0 +1,142 @@
+package com.store.controller.rest;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.store.model.Inventory;
+import com.store.model.MagazineLot;
+import com.store.service.InventoryService;
+import com.store.service.MagazineLotService;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author Bryan Baron
+ */
+@Controller
+@AllArgsConstructor
+public class MagazineLotController {
+
+	private MagazineLotService magazineLotServiceImpl;
+	private InventoryService inventoryServiceImpl;
+
+	/**
+	 * Creates a new magazine lot entity
+	 *
+	 * @param inventoryId id of the inventory where the magazine lot that is beeing
+	 * created will be placed
+	 * @param model holder model for context model's attributes.
+	 * @return the specified template view.
+	 */
+	@GetMapping("/magazine_lots/create/inventory={inventoryId}")
+	public String createMagazineLot(@PathVariable(name = "inventoryId")
+			Long inventoryId, Model model) {
+		model.addAttribute("action", "post");
+		model.addAttribute("inventoryId", inventoryId);
+		model.addAttribute("magazineLot", null);
+
+		return "magazineLotActions";
+	}
+
+	/**
+	 * Creates a new magazine lot entity
+	 *
+	 * @param newMagazineLot POJO with the information of the magazine lot
+	 * entity that is beeing created.
+	 * @param inventoryId id of the inventory where the magazine lot that is beeing
+	 * created will be placed
+	 * @return the specified template view.
+	 */
+	@GetMapping("/magazine_lots/manage/create/{inventoryId}")
+	public String createMagazineLot(MagazineLot newMagazineLot,
+			@PathVariable(name = "inventoryId") Long inventoryId) {
+		Inventory magazineLotInventory =
+			inventoryServiceImpl.getInventoryById(inventoryId).getBody();
+		Long branchOfficeId = magazineLotInventory.getBranchOffice().getId();
+		newMagazineLot.setMagazineLotInventory(magazineLotInventory);
+		magazineLotServiceImpl.createMagazineLot(newMagazineLot);
+
+		return String.format("redirect:/branch_offices/update/%d",
+				branchOfficeId);
+				
+	}
+
+	/**
+	 * Retrieves a magazine lot entity specified by isbn
+	 *
+	 * @param isbn isbn of the magazine lot entity that is beeing searched
+	 * @param model holder model for context model's attributes.
+	 * @return the specified template view
+	 */
+	@GetMapping("/magazine_lots/{isbn}")
+	public String showMagazineLot(@PathVariable(name = "isbn") UUID isbn,
+			Model model) {
+		MagazineLot magazineLot =
+			magazineLotServiceImpl.getMagazineLotByIsbn(isbn).getBody();
+
+		model.addAttribute("action", "get");
+		model.addAttribute("magazineLot", magazineLot);
+
+		return "magazineLotActions";
+	}
+
+	/**
+	 * Updates a magazine lot entity specified by isbn
+	 *
+	 * @param isbn isbn of the magazine lot entity that is beeing updated
+	 * @param model holder model for context model's attributes.
+	 * @return the specified template view
+	 */
+	@GetMapping("/magazine_lots/update/{isbn}")
+	public String updateMagazineLot(@PathVariable(name = "isbn") UUID isbn,
+			Model model) {
+		MagazineLot magazineLot =
+			magazineLotServiceImpl.getMagazineLotByIsbn(isbn).getBody();
+
+		model.addAttribute("action", "put");
+		model.addAttribute("magazineLot", magazineLot);
+
+		return "magazineLotActions";
+	}
+
+	/**
+	 * Updates a new magazine lot entity specified by isbn
+	 *
+	 * @param updatedMagazineLot POJO with the information of the magazine lot
+	 * entity that is beeing udpated.
+	 * @param isbn isbn of the magazine lot entity that is beeing updated
+	 * @return the specified template view.
+	 */
+	@GetMapping("/magazine_lots/manage/update/{isbn}")
+	public String updateMagazineLot(MagazineLot updatedMagazineLot,
+			@PathVariable(name = "isbn") UUID isbn) {
+		Long branchOfficeId = magazineLotServiceImpl.getMagazineLotByIsbn(isbn)
+			.getBody().getMagazineLotInventory().getBranchOffice().getId();
+		magazineLotServiceImpl.updateMagazineLotByIsbn(isbn, updatedMagazineLot);
+
+		return String.format("redirect:/branch_offices/update/%d",
+				branchOfficeId);
+
+	}
+
+	/**
+	 * Deletes a magazine lot entity specified by isbn
+	 *
+	 * @param isbn isbn of the magazine lot entity that is beeing deleted
+	 * @return the specified template view
+	 */
+	@GetMapping("/magazine_lots/manage/delete/{isbn}")
+	public String deleteMagazineLot(@PathVariable(name = "isbn") UUID isbn) {
+		Long branchOfficeId =
+			magazineLotServiceImpl.getMagazineLotByIsbn(isbn).getBody()
+				.getMagazineLotInventory().getBranchOffice().getId();
+		magazineLotServiceImpl.deleteMagazineLotByIsbn(isbn);
+
+		return String.format("redirect:/branch_offices/update/%d",
+				branchOfficeId);
+	}
+}
---
+++ src/main/java/com/store/controller/rest/SupervisorController.java
@@ -0,0 +1,88 @@
+package com.store.controller.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.store.exception.SupervisorNotFoundException;
+import com.store.model.Supervisor;
+import com.store.service.SupervisorService;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author Bryan Baron
+ */
+@Controller
+@AllArgsConstructor
+public class SupervisorController {
+
+	private SupervisorService supervisorServiceImpl;
+
+	/**
+	 * Shows the login page
+	 *
+	 * @param modelMap holder model for context model's attributes.
+	 * @return the login page template view.
+	 */
+	@GetMapping("/")
+	public String showLoginPage(ModelMap modelMap) {
+
+		if(modelMap.getAttribute("isLoged") != null)
+			modelMap.addAttribute("isLoged", false);
+		else
+			modelMap.addAttribute("isLoged", true);
+
+		return "loginPage";
+	}
+
+	/**
+	 * Shows the sign up page
+	 *
+	 * @return the sign up page template view.
+	 */
+	@GetMapping("/supervisor/create")
+	public String showSignUpPage() {
+		return "signUpPage";
+	}
+
+	/**
+	 * Creates a new supervisor entity
+	 *
+	 * @param newSupervisor POJO with the information of the supervisor entity
+	 * that is beeing created
+	 * @return the specified template view.
+	 */
+	@GetMapping("/supervisor/manage/create")
+	public String createSupervisor(Supervisor newSupervisor) {
+		supervisorServiceImpl.createSupervisor(newSupervisor);
+
+		return "redirect:/";
+	}
+
+	/**
+	 * Authenticates a supervisor by its username and password
+	 *
+	 * @param modelMap holder model for context model's attributes.
+	 * @param username username of the supervisor that is beeing authenticated
+	 * @param password password of the supervisor that is beeing authenticated
+	 * @return the login page template view.
+	 */
+	@GetMapping("/supervisor/manage/authenticate")
+	public ModelAndView authenticateSupervisor(ModelMap modelMap,
+			@RequestParam(name = "username") String username,
+			@RequestParam(name = "password") String password) {
+
+			try {
+				supervisorServiceImpl
+					.getSupervisorByUsernameAndPassword(username, password);
+			} catch(SupervisorNotFoundException e) {
+				modelMap.addAttribute("isLoged", false);
+				return new ModelAndView("loginPage", modelMap);
+			}
+
+		return new ModelAndView("redirect:/branch_offices");
+	}
+}
